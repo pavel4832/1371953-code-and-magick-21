7b1c694f990e3eb3dca2686130387db75976a4d7
Pavel Ulianov <paveluv@mail.ru>
2020-10-20 04:48:59 +0300

    Add webpack.

--- js/backend.js
+++ js/backend.js
@@ -1,47 +1,45 @@
 'use strict';
 
-(function () {
-  const LOAD_URL = `https://21.javascript.pages.academy/code-and-magick/data`;
-  const UPLOAD_URL = `https://21.javascript.pages.academy/code-and-magick`;
-  const StatusCode = {
-    OK: 200
-  };
-  const TIMEOUT_IN_MS = 10000;
+const LOAD_URL = `https://21.javascript.pages.academy/code-and-magick/data`;
+const UPLOAD_URL = `https://21.javascript.pages.academy/code-and-magick`;
+const StatusCode = {
+  OK: 200
+};
+const TIMEOUT_IN_MS = 10000;
 
-  window.backend = {
-    load: function (onSuccess, onError) {
-      const XHR = new XMLHttpRequest();
-      XHR.responseType = `json`;
+window.backend = {
+  load: function (onSuccess, onError) {
+    const XHR = new XMLHttpRequest();
+    XHR.responseType = `json`;
 
-      XHR.addEventListener(`load`, function () {
-        if (XHR.status === StatusCode.OK) {
-          onSuccess(XHR.response);
-        } else {
-          onError(`Статус ответа: ${XHR.status} ${XHR.statusText}`);
-        }
-      });
-      XHR.addEventListener(`error`, function () {
-        onError(`Произошла ошибка соединения`);
-      });
-      XHR.addEventListener(`timeout`, function () {
-        onError(`Запрос не успел выполниться за ${XHR.timeout}мс`);
-      });
-
-      XHR.timeout = TIMEOUT_IN_MS;
-
-      XHR.open(`GET`, LOAD_URL);
-      XHR.send();
-    },
-    upload: function (data, onSuccess) {
-      const XHR = new XMLHttpRequest();
-      XHR.responseType = `json`;
-
-      XHR.addEventListener(`load`, function () {
+    XHR.addEventListener(`load`, function () {
+      if (XHR.status === StatusCode.OK) {
         onSuccess(XHR.response);
-      });
+      } else {
+        onError(`Статус ответа: ${XHR.status} ${XHR.statusText}`);
+      }
+    });
+    XHR.addEventListener(`error`, function () {
+      onError(`Произошла ошибка соединения`);
+    });
+    XHR.addEventListener(`timeout`, function () {
+      onError(`Запрос не успел выполниться за ${XHR.timeout}мс`);
+    });
 
-      XHR.open(`POST`, UPLOAD_URL);
-      XHR.send(data);
-    }
-  };
-})();
+    XHR.timeout = TIMEOUT_IN_MS;
+
+    XHR.open(`GET`, LOAD_URL);
+    XHR.send();
+  },
+  upload: function (data, onSuccess) {
+    const XHR = new XMLHttpRequest();
+    XHR.responseType = `json`;
+
+    XHR.addEventListener(`load`, function () {
+      onSuccess(XHR.response);
+    });
+
+    XHR.open(`POST`, UPLOAD_URL);
+    XHR.send(data);
+  }
+};
--- js/dialog.js
+++ js/dialog.js
@@ -1,99 +1,97 @@
 'use strict';
 
-(function () {
-  const USER_DIALOG = document.querySelector(`.setup`);
-  const DIALOG_HANDLE = USER_DIALOG.querySelector('.upload');
-  const SETUP_OPEN = document.querySelector(`.setup-open`);
-  const SETUP_CLOSE = document.querySelector(`.setup-close`);
-  const FORM = document.querySelector(`.setup-wizard-form`);
-  const SETUP_USER_NAME = document.querySelector(`.setup-user-name`);
-  const START_X_POSITION = 50;
-  const START_Y_POSITION = 80;
+const USER_DIALOG = document.querySelector(`.setup`);
+const DIALOG_HANDLE = USER_DIALOG.querySelector('.upload');
+const SETUP_OPEN = document.querySelector(`.setup-open`);
+const SETUP_CLOSE = document.querySelector(`.setup-close`);
+const FORM = document.querySelector(`.setup-wizard-form`);
+const SETUP_USER_NAME = document.querySelector(`.setup-user-name`);
+const START_X_POSITION = 50;
+const START_Y_POSITION = 80;
 
-  const onPopupEscPress = function (evt) {
-    if (SETUP_USER_NAME !== document.activeElement) {
-      window.util.isEscEvent(evt, closePopup);
+const onPopupEscPress = function (evt) {
+  if (SETUP_USER_NAME !== document.activeElement) {
+    window.util.isEscEvent(evt, closePopup);
+  }
+};
+
+const openPopup = function () {
+  USER_DIALOG.classList.remove(`hidden`);
+
+  document.addEventListener(`keydown`, onPopupEscPress);
+};
+
+const closePopup = function () {
+  USER_DIALOG.classList.add(`hidden`);
+  USER_DIALOG.style.top = START_Y_POSITION + `px`;
+  USER_DIALOG.style.left = START_X_POSITION + `%`;
+
+  document.removeEventListener(`keydown`, onPopupEscPress);
+};
+
+SETUP_OPEN.addEventListener(`click`, openPopup);
+
+SETUP_OPEN.addEventListener(`keydown`, function (evt) {
+  window.util.isEnterEvent(evt, openPopup);
+});
+
+SETUP_CLOSE.addEventListener(`click`, closePopup);
+
+SETUP_CLOSE.addEventListener(`keydown`, function (evt) {
+  window.util.isEnterEvent(evt, closePopup);
+});
+
+DIALOG_HANDLE.addEventListener(`mousedown`, function (evt) {
+  evt.preventDefault();
+
+  let startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+  let dragged = false;
+
+  const onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    let shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    dragged = true;
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    USER_DIALOG.style.top = (USER_DIALOG.offsetTop - shift.y) + `px`;
+    USER_DIALOG.style.left = (USER_DIALOG.offsetLeft - shift.x) + `px`;
+  };
+
+  const onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener(`mousemove`, onMouseMove);
+    document.removeEventListener(`mouseup`, onMouseUp);
+
+    if (dragged) {
+      const onClickPreventDefault = function (clickEvt) {
+        clickEvt.preventDefault();
+        DIALOG_HANDLE.removeEventListener(`click`, onClickPreventDefault);
+      };
+
+      DIALOG_HANDLE.addEventListener(`click`, onClickPreventDefault);
     }
   };
 
-  const openPopup = function () {
-    USER_DIALOG.classList.remove(`hidden`);
+  document.addEventListener(`mousemove`, onMouseMove);
+  document.addEventListener(`mouseup`, onMouseUp);
+});
 
-    document.addEventListener(`keydown`, onPopupEscPress);
-  };
-
-  const closePopup = function () {
+FORM.addEventListener(`submit`, function (evt) {
+  window.backend.upload(new FormData(FORM), function () {
     USER_DIALOG.classList.add(`hidden`);
-    USER_DIALOG.style.top = START_Y_POSITION + `px`;
-    USER_DIALOG.style.left = START_X_POSITION + `%`;
-
-    document.removeEventListener(`keydown`, onPopupEscPress);
-  };
-
-  SETUP_OPEN.addEventListener(`click`, openPopup);
-
-  SETUP_OPEN.addEventListener(`keydown`, function (evt) {
-    window.util.isEnterEvent(evt, openPopup);
   });
-
-  SETUP_CLOSE.addEventListener(`click`, closePopup);
-
-  SETUP_CLOSE.addEventListener(`keydown`, function (evt) {
-    window.util.isEnterEvent(evt, closePopup);
-  });
-
-  DIALOG_HANDLE.addEventListener(`mousedown`, function (evt) {
-    evt.preventDefault();
-
-    let startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
-    };
-    let dragged = false;
-
-    const onMouseMove = function (moveEvt) {
-      moveEvt.preventDefault();
-
-      let shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
-      };
-
-      dragged = true;
-
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-
-      USER_DIALOG.style.top = (USER_DIALOG.offsetTop - shift.y) + `px`;
-      USER_DIALOG.style.left = (USER_DIALOG.offsetLeft - shift.x) + `px`;
-    };
-
-    const onMouseUp = function (upEvt) {
-      upEvt.preventDefault();
-
-      document.removeEventListener(`mousemove`, onMouseMove);
-      document.removeEventListener(`mouseup`, onMouseUp);
-
-      if (dragged) {
-        const onClickPreventDefault = function (clickEvt) {
-          clickEvt.preventDefault();
-          DIALOG_HANDLE.removeEventListener(`click`, onClickPreventDefault);
-        };
-
-        DIALOG_HANDLE.addEventListener(`click`, onClickPreventDefault);
-      }
-    };
-
-    document.addEventListener(`mousemove`, onMouseMove);
-    document.addEventListener(`mouseup`, onMouseUp);
-  });
-
-  FORM.addEventListener(`submit`, function (evt) {
-    window.backend.upload(new FormData(FORM), function () {
-      USER_DIALOG.classList.add(`hidden`);
-    });
-    evt.preventDefault();
-  });
-})();
+  evt.preventDefault();
+});
